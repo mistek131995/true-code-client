af63893ebb49fc5a73d8e5d76054eaa4d4986c7f
Юрий Кузнецов <omega131995@outlook.com>
2024-10-14 07:50:34 +0800

    Убрал наружение FSD

--- src/widgets/pagination/index.tsx
+++ src/widgets/pagination/index.tsx
@@ -2,14 +2,21 @@ import {observer} from "mobx-react-lite";
 import {Button, Flex, JustifyContent} from "@mistek/freedom-ui";
 import {getProductListStoreAsync, productListStore} from "../../features/product-list/product-list-store.ts";
 import styles from "./styles.module.scss"
+import {productListFilersStore} from "../../features/product-list-filers/product-list-filers-store.ts";
 
 export const Pagination = observer(() => {
     const arrayWithIndices = Array.from({ length: productListStore.pageCount }, (_, index) => index + 1);
 
+    const filters = {
+        name: productListFilersStore.name,
+        article: productListFilersStore.article,
+        sorting: productListFilersStore.sorting.toString()
+    }
+
     return <Flex justifyContent={JustifyContent.center} className={styles.paginationContainer}>
         {
             arrayWithIndices.map(page => {
-                return <Button key={page} onClick={() => getProductListStoreAsync(page)}>{page}</Button>
+                return <Button key={page} onClick={() => getProductListStoreAsync(filters, page)}>{page}</Button>
             })
         }
     </Flex>
