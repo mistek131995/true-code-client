a350f829da249931b3ecca008b831958eb794778
Юрий Кузнецов <omega131995@outlook.com>
2024-10-14 10:13:41 +0800

    Доработана пагинация

--- src/widgets/pagination/index.tsx
+++ src/widgets/pagination/index.tsx
@@ -1,22 +1,21 @@
 import {observer} from "mobx-react-lite";
 import {Button, Flex, JustifyContent} from "@mistek/freedom-ui";
-import {getProductListStoreAsync, productListStore} from "../../features/product-list/product-list-store.ts";
+import {productListStore} from "../../features/product-list/product-list-store.ts";
 import styles from "./styles.module.scss"
-import {productListFilersStore} from "../../features/product-list-filers/product-list-filers-store.ts";
+import {useSearchParams} from "react-router-dom";
 
 export const Pagination = observer(() => {
     const arrayWithIndices = Array.from({ length: productListStore.pageCount }, (_, index) => index + 1);
+    const [, setSearchParams] = useSearchParams()
 
-    const filters = {
-        name: productListFilersStore.name,
-        article: productListFilersStore.article,
-        sorting: productListFilersStore.sorting.toString()
+    const setPage = (page: number) => {
+        setSearchParams({page: page.toString()})
     }
 
     return <Flex justifyContent={JustifyContent.center} className={styles.paginationContainer}>
         {
             arrayWithIndices.map(page => {
-                return <Button key={page} onClick={() => getProductListStoreAsync(filters, page)}>{page}</Button>
+                return <Button key={page} onClick={() => setPage(page)}>{page}</Button>
             })
         }
     </Flex>
--- src/widgets/product-list/index.tsx
+++ src/widgets/product-list/index.tsx
@@ -6,8 +6,10 @@ import {Flex, JustifyContent} from "@mistek/freedom-ui";
 import styles from "./styles.module.scss"
 import {ProductCard} from "./ProductCard.tsx";
 import {productListFilersStore} from "../../features/product-list-filers/product-list-filers-store.ts";
+import {useSearchParams} from "react-router-dom";
 
 export const ProductList = observer(() => {
+    const [searchParams] = useSearchParams()
     const groupedProducts = productListStore.products?.reduce<Product[][]>((acc, product, index) => {
         if (index % 5 === 0) acc.push([]);
         acc[acc.length - 1].push(product);
@@ -19,8 +21,8 @@ export const ProductList = observer(() => {
             name: productListFilersStore.name,
             article: productListFilersStore.article,
             sorting: productListFilersStore.sorting.toString()
-        })
-    }, []);
+        }, Number(searchParams.get("page") || "1"))
+    }, [searchParams]);
 
     return groupedProducts?.map((group, groupIndex) => {
         return <Flex key={groupIndex} noWrap={true} className={styles.productsRow} justifyContent={JustifyContent.around}>
